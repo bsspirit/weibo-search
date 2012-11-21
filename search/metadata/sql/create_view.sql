4952a9a90616130892d859213585c767ff0114a0
bsspirit <bsspirit@163.com>
2012-11-21 18:20:14 +0800

    fans follow sort

--- search/metadata/sql/create_view.sql
+++ search/metadata/sql/create_view.sql
@@ -28,7 +28,8 @@ DROP VIEW v_fans;
 CREATE VIEW v_fans AS
 	SELECT r.fansid,u.screen_name,u.profile_image_url,u.followers_count,u.friends_count,u.statuses_count,u.verified,u.created_at,r.uid
 	FROM t_user u, t_user_relate r
-	WHERE  r.fansid = u.uid; /*and r.uid='1999250817'*/
+	WHERE  r.fansid = u.uid /*and r.uid='1999250817'*/
+	ORDER BY r.id DESC; 
 /*
  * select * from v_fans where uid='1999250817' limit 10;
  */	
@@ -37,7 +38,8 @@ DROP VIEW v_follows;
 CREATE VIEW v_follows AS
 	SELECT r.uid,u.screen_name,u.profile_image_url,u.followers_count,u.friends_count,u.statuses_count,u.verified,u.created_at,r.fansid
 	FROM t_user u, t_user_relate r
-	WHERE r.uid = u.uid; /*and r.fansid='1999250817'*/
+	WHERE r.uid = u.uid /*and r.fansid='1999250817'*/
+	ORDER BY r.id DESC; 
 /*
  * select * from v_follows where fansid='1999250817' limit 10;
  */	
